2f5554c5e146120479862b319da40b67a9ea4227
preistone <aion97@naver.com>
2024-12-23 17:36:31 +0900

    endpage design end

--- src/pages/quiz/Quizend.js
+++ src/pages/quiz/Quizend.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import { designFont } from "../../GlobalStyled";
+import bg from "../home/imgs/bg.jpg";
+import btn from "../home/imgs/btn.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 const Container = styled.div`
   max-width: 440px;
@@ -12,31 +16,107 @@ const Container = styled.div`
   position: relative;
   font-family: ${designFont.styleFont};
   text-align: center;
-  padding: 100px 0px;
+  /* padding: 100px 0px; */
   display: flex;
   flex-direction: column;
   align-items: center;
+  background: url(${bg}) no-repeat center / cover;
 
   h1 {
+    position: absolute;
+    top: 10%;
+    left: 50%;
+    transform: translateX(-50%);
     font-size: 30px;
-    margin-bottom: 50px;
   }
 
-  a {
+  /* a {
     width: 200px;
     height: 50px;
     margin: 50px 0;
+  } */
+
+  p {
+    position: absolute;
+    top: 20%;
+    left: 50%;
+    width: 100%;
+    transform: translateX(-50%);
+    font-size: 20px;
   }
 `;
 
-const HomeBtn = styled.div`
-  width: 200px;
-  height: 50px;
-  border-radius: 10px;
-  line-height: 50px;
-  /* margin: 50px auto 0 auto; */
-  background-color: salmon;
+// const HomeBtn = styled.div`
+//   width: 200px;
+//   height: 50px;
+//   border-radius: 10px;
+//   line-height: 50px;
+//   /* margin: 50px auto 0 auto; */
+//   background-color: salmon;
+// `;
+
+const NextBtn3 = styled.div`
+  width: 100%;
+  border-radius: 20px;
+  overflow: hidden;
+  height: 200px;
+  position: relative;
+  top: 30%;
+  font-family: ${designFont.styleFont};
+
+  img {
+    width: 100%;
+    max-height: 200px;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 50;
+  }
+
+  h2 {
+    font-size: 30px;
+    z-index: 100;
+    position: absolute;
+    top: 24%;
+    left: 24%;
+
+    &:hover + div {
+      display: block;
+    }
+  }
+
+  h3 {
+    font-size: 30px;
+    z-index: 100;
+    position: absolute;
+    top: 56%;
+    left: 24%;
+
+    &:hover + div {
+      display: block;
+    }
+  }
 `;
+
+const Dot1 = styled.div`
+  font-size: 24px;
+  position: absolute;
+  top: 24%;
+  left: 14%;
+  z-index: 200;
+  display: none;
+`;
+
+const Dot2 = styled.div`
+  font-size: 24px;
+  position: absolute;
+  top: 56%;
+  left: 14%;
+  z-index: 200;
+  display: none;
+`;
+
 const Quizend = () => {
   const location = useLocation();
   const score = location.state?.score || 0; // score값 받기
@@ -47,12 +127,27 @@ const Quizend = () => {
     <Container>
       <h1>퀴즈 종료</h1>
       <p>당신은 총 {score}개의 문제를 맞췄습니다!</p>
-      <Link to={"/"}>
+      <NextBtn3>
+        <img src={btn} alt="버튼이미지" />
+        <Link to={"/"}>
+          <h2>홈으로로</h2>
+          <Dot1>
+            <FontAwesomeIcon icon={faPlay} />
+          </Dot1>
+        </Link>
+        <Link to={"/quiz"}>
+          <h3>다시하기</h3>
+          <Dot2>
+            <FontAwesomeIcon icon={faPlay} />
+          </Dot2>
+        </Link>
+      </NextBtn3>
+      {/* <Link to={"/"}>
         <HomeBtn>홈으로</HomeBtn>
       </Link>
       <Link to={"/quiz"}>
         <HomeBtn>다시하기</HomeBtn>
-      </Link>
+      </Link> */}
     </Container>
   );
 };
